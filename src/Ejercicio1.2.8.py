4155506ded8b84a7517b5750bf31837526de797e
JoseAntonio <jdiabus511@g.educaand.es>
2024-09-30 16:29:10 +0200

    Ejercicios 20-24 e intermedios

--- src/Ejercicio1.2.8.py
+++ src/Ejercicio1.2.8.py
@@ -1,3 +1,6 @@
 """
 Escribir el programa del ejercicio 1.7 usando solamente dos variables diferentes.
 """
+num1 = int(input("Introduce un número cualquiera: "))
+num2 = int(input("Introduce otro número: "))
+print("La suma es:", num1 + num2 + int(input("Introduce un tercer número: ")))
--- src/Ejercicio1.2.9.py
+++ src/Ejercicio1.2.9.py
@@ -1,3 +1,4 @@
 """
 ¿Es posible escribir el programa del ejercicio 1.7 sin usar variables? Inténtalo.
 """
+print("La suma es:", int(input("Introduce un número: ")) + int(input("Introduce otro número: ")) + int(input("Introduce un último número: ")))
