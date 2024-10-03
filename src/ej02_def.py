999791eb72103bad945424a93bbb490dcaf94695
JoseAntonio <jdiabus511@g.educaand.es>
2024-10-03 13:05:06 +0200

    Ejercicios de funciones plateo

--- src/ej02_def.py
+++ src/ej02_def.py
@@ -2,6 +2,20 @@
 Escribe un programa para pedirle al usuario las horas de trabajo y
 el precio por hora y calcule el importe total del servicio.
 """
-horas = int(input("Horas de trabajo: "))
-coste = int(input("Coste por hora: "))
-print("Importe: "+str(horas*coste))
+def horas():
+    return int(input("Introduce las horas de trabajo: "))
+
+def coste():
+    return float(input("Introduce el coste por hora: "))
+
+def importe(importe):
+    print("Importe total: "+str(importe))
+
+def main():
+    horas_total = horas()
+    coste_total = coste()
+    importe_total = horas_total * coste_total
+    importe(importe_total)
+
+if __name__ == '__main__':
+    main()
--- src/ej04_def.py
+++ src/ej04_def.py
@@ -1,7 +1,7 @@
 """
 Escribe un programa que le pida al usuario una temperatura en grados Celsius,
 la convierta a grados Fahrenheit e imprima por pantalla la temperatura convertida.
-"""
 celsius = int(input("Ingrese el temperatura en grados Celsius: "))
 fahrenheit = (celsius * 9 /5) + 32
 print("La temperatura convertida es: "+str(fahrenheit)+"Fº.")
+"""
