6baef5d9cace0ed4fc31802bd6678831f1a11057
JoseAntonio <jdiabus511@g.educaand.es>
2024-10-15 19:40:45 +0200

    Actualizacion reto

--- src/reto_adivinar_num.py
+++ src/reto_adivinar_num.py
@@ -1,50 +1,27 @@
 import random
-"""
-objetivo = random.randint(0, 100)
 
-num = int(input("Intenta adivinar un numero del 0 al 100: "))
-
-while num != objetivo:
-    if num > objetivo:
-        dif = num - objetivo
-        if dif <=10:
-            print("Caliente caliente, estás cerca del objetivo")
-        elif dif>=25:
-            print("Frío frío, estás lejos del objetivo")
-        else:
-            print("El numero es mayor que el objetivo")
-        num = int(input("Prueba otra vez: "))
-    elif num < objetivo:
-        dif = objetivo - num
-        if dif <= 10:
-            print("Caliente caliente, estás cerca del objetivo")
-        elif dif >= 25:
-            print("Frío frío, estás lejos del objetivo")
-        else:
-            print("El numero es menor que el objetivo")
-        num = int(input("Prueba otra vez: "))
-    else:
-        print("Eso no es un numero")
-        num = int(input("Prueba otra vez: "))
-"""
 def pedirnumero():
-    return int(input("Intenta adivinar un numero del 0 al 100: "))
+    return int(input("\nIntenta adivinar un numero del 0 al 100: "))
 
 def comprobar_numero():
     num = pedirnumero()
     while num >=0 or num <= 100:
         if num <0:
-            print("Sólo puedes introducir de 0 al 100!!!")
+            print("Sólo puedes introducir de 0 al 100!!!\n")
             num = pedirnumero()
         elif num >100:
-            print("Sólo puedes introducir de 0 al 100!!!")
+            print("Sólo puedes introducir de 0 al 100!!!\n")
             num = pedirnumero()
         else:
             return num
 
-"""
-def diferencial():
-"""
+
+def diferencial(objetivo: random, numero: int):
+    if objetivo > numero:
+        return objetivo-numero
+    elif objetivo < numero:
+        return numero-objetivo
+
 
 def main():
     objetivo = random.randint(0, 100)
@@ -52,16 +29,32 @@ def main():
     assist = False
     while not assist:
         numero = comprobar_numero()
+        dif = diferencial(objetivo, numero)
         if numero > objetivo:
-            print("El numero es mayor que el objetivo")
+            if dif <= 3:
+                print("Te estás quemando, el objetivo está muy cerca.")
+            elif dif <= 10:
+                print("Caliente caliente, estás cerca del objetivo.")
+            elif dif >= 25:
+                print("Frío frío, estás lejos del objetivo.")
+            else:
+                print("El numero es mayor que el objetivo.")
             intentos += 1
         elif numero < objetivo:
-            print("El numero es menor que el objetivo")
+            if numero < objetivo:
+                if dif <= 3:
+                    print("Te estás quemando, el objetivo está muy cerca.")
+                elif dif <= 10:
+                    print("Caliente caliente, estás cerca del objetivo.")
+                elif dif >= 25:
+                    print("Frío frío, estás lejos del objetivo.")
+                else:
+                    print("El numero es menor que el objetivo.")
             intentos += 1
         else:
-            print("LO HAS CONSEGUIDO!!!")
+            print("\nLO HAS CONSEGUIDO!!!")
             assist = True
-    print("Has superado el reto en", intentos, "intentos")
+    print("Has superado el reto en", intentos, "intentos.")
 
 
 if __name__ == "__main__":
